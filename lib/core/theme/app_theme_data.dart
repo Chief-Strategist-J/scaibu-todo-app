b4f2d75dfb36ee15048af76f25840a85f1922e7f
cheif.strategist.j <cheif.strategist.j@gmail.com>
2024-06-18 09:17:34 +0530

    - minor changes

--- lib/core/theme/app_theme_data.dart
+++ lib/core/theme/app_theme_data.dart
@@ -38,6 +38,68 @@ class AppThemeData {
         enabledBorder: _getInputBoarder,
         focusedBorder: _getInputBoarder,
       ),
+      pageTransitionsTheme: const PageTransitionsTheme(
+        builders: {
+          TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+          TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+        },
+      ),
     );
   }
+}
+
+class MyTransitions {
+  static Route<dynamic> fadeTransition(Widget page) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+      },
+    );
+  }
+}
+
+
+class FadePageRoute<T> extends PageRouteBuilder<T> {
+  final WidgetBuilder builder;
+
+  FadePageRoute({required this.builder})
+      : super(
+          pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+            return builder(context);
+          },
+          transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        );
+}
+
+class CustomPageRoute<T> extends PageRouteBuilder<T> {
+  final WidgetBuilder builder;
+  final Duration duration;
+  final Curve curve;
+  final bool opaque;
+
+  CustomPageRoute({
+    required this.builder,
+    this.duration = const Duration(milliseconds: 300),
+    this.curve = Curves.ease,
+    this.opaque = true,
+  }) : super(
+          pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) => builder(context),
+          transitionDuration: duration,
+          transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+          opaque: opaque,
+        );
 }
